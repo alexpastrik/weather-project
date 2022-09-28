e751cc2af17317aa7d3070fec64427b7084fb8f0
alexpastrik <102768664+alexpastrik@users.noreply.github.com>
2022-09-28 15:22:59 -0400

    Added Bootstrap

--- src/App.js
+++ src/App.js
@@ -5,20 +5,25 @@ import "./App.css";
 export default function App() {
   return (
     <div className="App">
-      Weather Project
-      <footer>
-        This project is coded by{" "}
-        <a href="https://stalwart-croissant-f1908a.netlify.app" target="_blank">
-          Alex Pastrik
-        </a>{" "}
-        and is {""}
-        <a
-          href="https://github.com/alexpastrik/weather-project"
-          target="_blank"
-        >
-          open-sourced on GitHub
-        </a>
-      </footer>
+      <div className="container">
+        Weather Project
+        <footer>
+          This project is coded by{" "}
+          <a
+            href="https://stalwart-croissant-f1908a.netlify.app"
+            target="_blank"
+          >
+            Alex Pastrik
+          </a>{" "}
+          and is {""}
+          <a
+            href="https://github.com/alexpastrik/weather-project"
+            target="_blank"
+          >
+            open-sourced on GitHub
+          </a>
+        </footer>
+      </div>
     </div>
   );
 }
