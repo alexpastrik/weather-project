95ddcccbf9c7222ee7688945fed0f96f286accdd
alexpastrik <102768664+alexpastrik@users.noreply.github.com>
2022-09-28 14:19:52 -0400

    added GitHub link and portfolio link

--- src/App.js
+++ src/App.js
@@ -2,8 +2,23 @@ import React from "react";
 
 import "./App.css";
 
-function App() {
-  return <div className="App">Weather Project</div>;
+export default function App() {
+  return (
+    <div className="App">
+      Weather Project
+      <footer>
+        This project is coded by{" "}
+        <a href="https://stalwart-croissant-f1908a.netlify.app" target="_blank">
+          Alex Pastrik
+        </a>{" "}
+        and is {""}
+        <a
+          href="https://github.com/alexpastrik/weather-project"
+          target="_blank"
+        >
+          open-sourced on GitHub
+        </a>
+      </footer>
+    </div>
+  );
 }
-
-export default App;
