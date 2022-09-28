8a83c6af93e25546b09896ed553e65cfcd142e8b
alexpastrik <102768664+alexpastrik@users.noreply.github.com>
2022-09-28 18:00:28 -0400

    changing css of app

--- src/Weather.js
+++ src/Weather.js
@@ -2,5 +2,43 @@ import React from "react";
 import "./Weather.css";
 
 export default function Weather() {
-  return <div className="Weather">Hello from weather</div>;
+  return (
+    <div className="Weather">
+      <form>
+        <div className="row">
+          <div className="col-9">
+            <input
+              type="search"
+              placeholder="Enter a city"
+              className="form-control"
+            />
+          </div>
+          <div className="col-3">
+            <input type="submit" value="search" className="btn btn-primary" />
+          </div>
+        </div>
+      </form>
+      <h1>New York</h1>
+      <ul>
+        <li>Wednesday 8:00</li>
+        <li>mostly cloudy</li>
+      </ul>
+      <div className="row">
+        <div className="col-4">
+          <img
+            src="https://ssl.gstatic.com/onebox/weather/64/partly_cloudy.png"
+            alt="mostly cloudy"
+          />
+          6°C
+        </div>
+        <div className="col-6">
+          <ul>
+            <li>precipitation: 12%</li>
+            <li>humidity: 70%</li>
+            <li>Wind: 13km/h</li>
+          </ul>
+        </div>
+      </div>
+    </div>
+  );
 }
