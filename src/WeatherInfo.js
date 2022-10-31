ce67ad56ee8dcf1bc1baf7fcb5318e592f519423
alexpastrik <102768664+alexpastrik@users.noreply.github.com>
2022-10-31 13:28:48 -0400

    added new weather api

--- src/WeatherInfo.js
+++ src/WeatherInfo.js
@@ -1,5 +1,6 @@
 import React from "react";
 import FormattedDate from "./FormattedDate";
+import WeatherIcon from ".WeatherIcon";
 
 export default function WeatherInfo(props) {
   return (
@@ -13,7 +14,21 @@ export default function WeatherInfo(props) {
       </ul>
       <div className="row mt-3">
         <div className="col-6">
-          <div className="d-flex"></div>
+          <div className="clearfix">
+            <WeatherIcon code={props.data.icon} />
+            <img
+              src={props.data.iconUrl}
+              alt={props.data.description}
+              className="float-left"
+            />
+
+            <div className="float-left">
+              <span className="temperature">
+                {Math.round(props.data.temperature)}
+              </span>
+              <span className="unit">°C</span>
+            </div>
+          </div>
         </div>
         <div className="col-6">
           <ul>
